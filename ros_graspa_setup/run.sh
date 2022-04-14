aac9f436f5cb29128d4ef7f0f68a3accf6d7b5ec
fbottarel <fabrizio.bottarel@iit.it>
2022-04-14 15:39:16 +0200

    removed sudo instructions from run.sh

--- ros_graspa_setup/run.sh
+++ ros_graspa_setup/run.sh
@@ -3,7 +3,7 @@
 # Expose the X server on the host.
 # This only works if the user is root though!
 
-sudo xhost +local:root
+xhost +local:root
 # --rm: Make the container ephemeral (delete on exit).
 # -it: Interactive TTY.
 # --gpus all: Expose all GPUs to the container.
@@ -15,4 +15,4 @@ docker run \
   -e QT_X11_NO_MITSHM=1 \
   hsp-panda/ros_graspa_setup:melodic
 
-sudo xhost -local:root
+xhost -local:root
