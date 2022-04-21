3c768e9b32a63d4732ffd6d9280b9503ccc2f080
fbottarel <fabrizio.bottarel@iit.it>
2022-04-21 17:04:02 +0200

    added dockerfile instructions for generic panda image

---
+++ ros_panda_setup/ros_panda_setup/run.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# Expose the X server on the host.
+# This only works if the user is root though!
+
+ROBOT_IP = 172.16.0.2
+TABLE_HEIGHT = 0.0
+
+xhost +local:root
+# --rm: Make the container ephemeral (delete on exit).
+# -it: Interactive TTY.
+# --gpus all: Expose all GPUs to the container.
+docker run \
+  -it \
+  -v /tmp/.X11-unix:/tmp/.X11-unix \
+  -v /dev:/dev \
+  -e DISPLAY=$DISPLAY \
+  -e QT_X11_NO_MITSHM=1 \
+  --network=host \
+  --privileged \
+  hsp-panda/ros_panda_setup:melodic \
+  'roslaunch panda_grasp_server grasp_server.launch \
+                                robot_ip:='"${ROBOT_IP}"' \
+                                table_height:='"${TABLE_HEIGHT}"''
+
+xhost -local:root
